0b5bf6e15d24ed6bc6e4ffc0950ac5ed99b507f5
a-als <a_al-s@mail.com>
2024-06-07 07:08:07 +0300

    hw05 added

---
+++ assignmets/hw05/carousel.js
@@ -0,0 +1,55 @@
+const photos = [
+    "images/img1-600x400.jpg",
+    "images/img2-600x400.jpg",
+    "images/img3-600x400.jpg",
+    "images/img4-600x400.jpg",
+    "images/img5-600x400.jpg",
+    "images/img6-600x400.jpg",
+    "images/img7-600x400.jpg",
+    "images/img8-600x400.jpg",
+    "images/img9-600x400.jpg",
+    "images/img10-600x400.jpg"
+];
+let idx = 0;
+
+
+/* This function should:
+    (1) display the new image inside of the .current-photo container, and 
+    (2) update the caption inside of the .caption paragraph
+*/
+const currentPhoto = document.querySelector(".current-photo");
+const caption = document.querySelector(".caption");
+
+function showImage() {
+    // check the idx
+    currentPhoto.innerHTML = `
+    <img src="${photos[idx]}" alt="current photo" />
+    `;
+    caption.innerHTML = `Image ${idx + 1} of ${photos.length}`;
+}
+
+
+/* This function should set the idx variable 
+   to one greater than the current value of idx, 
+   and then invoke the showImage() function.
+   If the idx gets to one less than the length of 
+   the array, set idx to 0.
+*/
+function forward() {
+    idx++;
+    if (idx >= photos.length) {idx = 0;}
+    showImage();
+}
+
+
+/* This function should set the idx variable 
+   to one less than the current value of idx, 
+   and then invoke the showImage() function.
+   If the idx gets to the beginning, set idx to
+   one less than the length of the array.
+*/
+function back() {
+    idx--;
+    if (idx < 0) {idx = photos.length - 1;}
+    showImage();
+}
